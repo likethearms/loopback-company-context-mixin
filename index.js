67934508b7e31dc9c296f0f5485c1be502c38664
Henrik Salmela <henrik.salmela@gmail.com>
2019-01-25 20:15:22 +0200

    Init

---
+++ index.js
@@ -0,0 +1,24 @@
+module.exports = (Model, options) => {
+  Model.beforeRemote('*', (ctx, _, next) => {
+    const writeMethodNames = [
+      'create',
+      'replaceOrCreate',
+      'patchOrCreate',
+    ];
+    console.log('CompanyContext');
+
+    if (writeMethodNames.includes(ctx.method.name)) return next();
+    if (options
+      && 'ignore' in options
+      && options.ignore.includes(ctx.method.name)) return next();
+    if (!ctx.args.filter) ctx.args.filter = {};
+    if (!ctx.args.where) ctx.args.where = {};
+    if (!ctx.args.filter.where) ctx.args.filter.where = {};
+    const { userId } = ctx.args.options.accessToken;
+    return Model.app.models.Member.findById(userId, (err, user) => {
+      ctx.args.filter.where.companyId = user.companyId;
+      ctx.args.where.companyId = user.companyId;
+      next();
+    });
+  });
+};
